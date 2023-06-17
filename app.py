945003d93d0d8c8c79e9e9ef141ec670aede2641
Leandro Garcia dos Santos <124471771+LeandroGarSantos@users.noreply.github.com>
2023-06-17 11:22:04 -0700

    Update app.py

--- app.py
+++ app.py
@@ -47,6 +47,14 @@ def update_country():
         return redirect('/all-users')
     else:
         return render_template ('update_country.html')
+    
+    @app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return render_template('500.html'), 500
 
 
 if __name__ == "__main__":
